2fcfa7d33cc7bc87667c899bdfdb969b8ac8f0a4
take088917761@gmail.com <yuyalikeswimming@icloud.com>
2025-12-10 12:50:40 +0900

    メール機能調整

--- includes/functions.php
+++ includes/functions.php
@@ -2,3 +2,9 @@
 function h($str){
   return htmlspecialchars($str, ENT_QUOTES,'utf-8');
 }
+
+function setToken(){
+  $csrf_token = bin2hex(random_bytes(32));
+  $_SESSION['csrf_token'] = $csrf_token;
+  return $csrf_token;
+}
